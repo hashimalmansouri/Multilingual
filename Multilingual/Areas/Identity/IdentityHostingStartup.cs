96374d8c427a91c006aed31d708634e049a822aa
هاشم المنصوري <HashimAlmansouri@outlook.com>
2021-07-11 10:06:22 +0300

    add identity login and register

---
+++ Multilingual/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Multilingual.Data;
+
+[assembly: HostingStartup(typeof(Multilingual.Areas.Identity.IdentityHostingStartup))]
+namespace Multilingual.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
