0131bcce88bdd711910f686a6bf691a87bf3eaa7
Hashim Al-Mansouri <55322650+hashimalmansouri@users.noreply.github.com>
2024-02-14 20:07:10 +0300

    -

---
+++ Multilingual/Helpers/CultureHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace Multilingual.Helpers
+{
+    public static class CultureHelper
+    {
+        public static bool IsArabic()
+        {
+            bool isArabic = false;
+            var currentCulture = CultureInfo.CurrentUICulture.Name;
+            if (currentCulture == "ar")
+            {
+                isArabic = true;
+            }
+            return isArabic;
+        }
+    }
+}
